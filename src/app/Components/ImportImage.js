c4fb80a8177e50765df5998621c90158573428bb
davidscottconrad <david.scott.conrad@gmail.com>
2025-09-26 13:13:33 -0700

    image component

---
+++ src/app/Components/ImportImage.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function ImportImage() {
+  return (
+    <div>
+      <div>
+        <h1>Smocking Help</h1>
+        <h2>Add and image of your thread, pattern, and fabric choice. We can show you what it might look like when its done. </h2>
+      </div>
+      Import Image
+    </div>
+  );
+}
