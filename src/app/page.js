e252a0ffc63e529b361cbaa63e0c4ff925870a3f
davidscottconrad <david.scott.conrad@gmail.com>
2025-09-26 13:46:05 -0700

    remove hello world

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Header from "./Components/Header.js"
 export default function Home() {
   return (
-   <div>Hello World
+   <div>
     <Header></Header>
    </div>
 
